9831a38cf85ace47063805f8adbe92f2d36a383a
Luzifer <mrcookiesmedia@gmail.com>
2020-06-28 20:09:59 +0200

    Update 0.2.2

--- data.lua
+++ data.lua
@@ -1,18 +1,17 @@
 require "config"
 require "util"
 
-local m = "__colored_blueprints__"
+local MODNAME = "__colored_blueprints__"
 
 data:extend
 {
-	{ type = "item-group", name = "colored-blueprints", icon = m .. "/graphics/icon.png", icon_size = 64, order = "gg" },
+	{ type = "item-group", name = "colored-blueprints", icon = MODNAME .. "/graphics/icon.png", icon_size = 64, order = "gg" },
 	{ type = "item-subgroup", name = "colored-blueprint", group = "colored-blueprints", order = "a" },
 	{ type = "item-subgroup", name = "colored-book", group = "colored-blueprints", order = "b" }
 }
 
-local n = {}
-local o = 0
-
+local Order = 0
+local Placeholder = ""
 local color_vanilla = "#1A78AB"
 
 data.raw["blueprint"]["blueprint"].icon = nil
@@ -20,8 +19,8 @@ data.raw["blueprint"]["blueprint"].icon_size = nil
 data.raw["blueprint"]["blueprint"].icon_mimap = nil
 data.raw["blueprint"]["blueprint"].icons =
 {
-	{ icon = m .. "/graphics/blueprint-base.png", icon_size = 32 },
-	{ icon = m .. "/graphics/blueprint-mask.png", icon_size = 32, tint = util.color( color_vanilla ) }
+	{ icon = MODNAME .. "/graphics/blueprint-base.png", icon_size = 32 },
+	{ icon = MODNAME .. "/graphics/blueprint-mask.png", icon_size = 32, tint = util.color( color_vanilla ) }
 }
 data.raw["blueprint"]["blueprint"].localised_name = { "colored.colored-blueprints", color_vanilla }
 
@@ -30,55 +29,59 @@ data.raw["blueprint-book"]["blueprint-book"].icon_size = nil
 data.raw["blueprint-book"]["blueprint-book"].icon_mimap = nil
 data.raw["blueprint-book"]["blueprint-book"].icons =
 {
-	{ icon = m .. "/graphics/blueprint-book-base.png", icon_size = 32 },
-	{ icon = m .. "/graphics/blueprint-book-mask.png", icon_size = 32, tint = util.color( color_vanilla ) }
+	{ icon = MODNAME .. "/graphics/blueprint-book-base.png", icon_size = 32 },
+	{ icon = MODNAME .. "/graphics/blueprint-book-mask.png", icon_size = 32, tint = util.color( color_vanilla ) }
 }
 data.raw["blueprint-book"]["blueprint-book"].localised_name = { "colored.colored-books", color_vanilla }
 
---local s = settings.startup["new-colors"].value
+for HEX, _ in pairs( Senpais.BookColors ) do
+	if Order < 10 then Placeholder = "0" else Placeholder = "" end
+	Order = Order + 1
 
-local Colors = {}
-
---[[if s ~= "" then
-	forword in string.gmatch( s, "#[0-9a-fA-F]+" ) do log( word ) end
-end]]
-
-for h, _ in pairs( Senpais.BookColors ) do
-	Colors[h] = true
-end
-
-for h, _ in pairs( Colors ) do
-	if o < 10 then p = "0" else p = "" end
-	o = o + 1
-
-	local np = util.table.deepcopy( data.raw["blueprint"]["blueprint"] )
-	np.name = h .. "-colored-blueprint"
-	np.icon = nil
-	np.icon_size = nil
-	np.icon_mimap = nil
-	np.icons =
+	local newblueprint = util.table.deepcopy( data.raw["blueprint"]["blueprint"] )
+	newblueprint.name = HEX .. "-colored-blueprint"
+	newblueprint.icon = nil
+	newblueprint.icon_size = nil
+	newblueprint.icon_mimap = nil
+	newblueprint.icons =
 	{
-		{ icon = m .. "/graphics/blueprint-base.png", icon_size = 32 },
-		{ icon = m .. "/graphics/blueprint-mask.png", icon_size = 32, tint = util.color( h ) }
+		{ icon = MODNAME .. "/graphics/blueprint-base.png", icon_size = 32 },
+		{ icon = MODNAME .. "/graphics/blueprint-mask.png", icon_size = 32, tint = util.color( HEX ) }
 	}
-	np.subgroup = "colored-blueprint"
-	np.order = "print-" .. p .. o
-	np.localised_name = { "colored.colored-blueprints", h }
-	np.localised_description = { "item.description.blueprint" }
+	newblueprint.subgroup = "colored-blueprint"
+	newblueprint.order = "print-" .. Placeholder .. Order
+	newblueprint.localised_name = { "colored.colored-blueprints", HEX }
+	newblueprint.localised_description = { "item.description.blueprint" }
 
-	local nb = util.table.deepcopy( data.raw["blueprint-book"]["blueprint-book"] )
-	nb.name = h .. "-colored-book"
-	nb.icon = nil
-	nb.icon_size = nil
-	nb.icon_mimap = nil
-	nb.icons =
+	local newblueprintrecipe =
 	{
-		{ icon = m .. "/graphics/blueprint-book-base.png", icon_size = 32 },
-		{ icon = m .. "/graphics/blueprint-book-mask.png", icon_size = 32, tint = util.color( h ) }
+		type = "recipe",
+		name = HEX .. "-colored-blueprint",
+		ingredients = {},
+		result = HEX .. "-colored-blueprint"
 	}
-	nb.subgroup = "colored-book"
-	nb.order = "book-" .. p .. o
-	nb.localised_name = { "colored.colored-books", h }
+
+	local newblueprintbook = util.table.deepcopy( data.raw["blueprint-book"]["blueprint-book"] )
+	newblueprintbook.name = HEX .. "-colored-book"
+	newblueprintbook.icon = nil
+	newblueprintbook.icon_size = nil
+	newblueprintbook.icon_mimap = nil
+	newblueprintbook.icons =
+	{
+		{ icon = MODNAME .. "/graphics/blueprint-book-base.png", icon_size = 32 },
+		{ icon = MODNAME .. "/graphics/blueprint-book-mask.png", icon_size = 32, tint = util.color( HEX ) }
+	}
+	newblueprintbook.subgroup = "colored-book"
+	newblueprintbook.order = "book-" .. Placeholder .. Order
+	newblueprintbook.localised_name = { "colored.colored-books", HEX }
 	
-	data:extend{ np, nb }
+	local newblueprintbookrecipe =
+	{
+		type = "recipe",
+		name = HEX .. "-colored-book",
+		ingredients = {},
+		result = HEX .. "-colored-book"
+	}
+
+	data:extend{ newblueprint, newblueprintrecipe, newblueprintbook, newblueprintbookrecipe }
 end
